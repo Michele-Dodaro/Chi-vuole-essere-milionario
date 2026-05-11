c41f86aac5770303c13d203938e37badc11c1006
Dodaro Michele <michele.dodaro@itsincom.it>
2026-05-11 15:47:55 +0200

    ADD: domande nello script

---
+++ script.ts
@@ -0,0 +1,39 @@
+const questions = [
+    {
+        question: "Quale animale è il simbolo della città di Roma?",
+        answers: ["Lupa", "Aquila", "Leone", "Gatto"],
+        correct: 0
+    },
+    {
+        question: "Quanti minuti ci sono in un'ora?",
+        answers: ["50", "100", "60", "120"],
+        correct: 2
+    },
+    {
+        question: "In quale continente si trova il deserto del Sahara?",
+        answers: ["Asia", "America", "Australia", "Africa"],
+        correct: 3
+    },
+    {
+        question: "Qual è il metallo rappresentato dal simbolo 'Au'?",
+        answers: ["Argento", "Oro", "Alluminio", "Rame"],
+        correct: 1
+    },
+    {
+        question: "Chi ha scritto 'La Divina Commedia'?",
+        answers: ["Alessandro Manzoni", "Giacomo Leopardi", "Dante Alighieri", "Ugo Foscolo"],
+        correct: 2
+    },
+    {
+        question: "Qual è l'organo più grande del corpo umano?",
+        answers: ["Cervello", "Pelle", "Fegato", "Polmoni"],
+        correct: 1
+    },
+    {
+        question: "In che anno è avvenuta la caduta del Muro di Berlino?",
+        answers: ["1989", "1991", "1985", "1945"],
+        correct: 0
+    }
+];
+
+let currentQuestionIndex = 0;
